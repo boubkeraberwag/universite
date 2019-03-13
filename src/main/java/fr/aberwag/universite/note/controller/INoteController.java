bb9988c95a9a7fbf94909cdbc158d2c451353aba
Boubker ABERWAG <b_aberwag@yahoo.fr>
2019-03-13 11:39:15 +0100

    add Note

--- src/main/java/fr/aberwag/universite/note/controller/INoteController.java
+++ src/main/java/fr/aberwag/universite/note/controller/INoteController.java
@@ -4,4 +4,6 @@ import org.springframework.ui.Model;
 
 public interface INoteController {
 	public String findAll(Model model);
+	
+	public String addNote(Model model);
 }
--- src/main/java/fr/aberwag/universite/note/controller/impl/NoteControllerImpl.java
+++ src/main/java/fr/aberwag/universite/note/controller/impl/NoteControllerImpl.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import fr.aberwag.universite.etudiant.domain.Etudiant;
+import fr.aberwag.universite.etudiant.service.impl.EtudiantServiceImpl;
+import fr.aberwag.universite.matiere.domain.Matiere;
+import fr.aberwag.universite.matiere.service.impl.MatiereServiceImpl;
 import fr.aberwag.universite.note.controller.INoteController;
 import fr.aberwag.universite.note.domain.Note;
 import fr.aberwag.universite.note.service.impl.NoteServiceImpl;
@@ -16,12 +20,31 @@ public class NoteControllerImpl implements INoteController{
 	@Autowired
 	NoteServiceImpl noteService;
 	
+	@Autowired
+	EtudiantServiceImpl etudiantService;
+	@Autowired
+	MatiereServiceImpl matiereService;
+	
 	@Override
 	@GetMapping("/notes")
 	public String findAll(Model model) {
 		List<Note> listeNotes = noteService.findAll();
 		model.addAttribute("notes", listeNotes);
 		return "note/notes";
+	}
+
+	@Override
+	@GetMapping("/note-formulaire-add")
+	public String addNote(Model model) {
+		List<Etudiant> listeEtudiant = etudiantService.findAll();
+		model.addAttribute("etudiants", listeEtudiant);
+		
+		List<Matiere> listeMatieres = matiereService.findAll();
+		model.addAttribute("matieres", listeMatieres);
+		
+		Note note = new Note();
+		model.addAttribute("note", note);
+		return "note/add-note-formulaire";
 	}
 
 }
